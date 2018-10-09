fdd6507c61d0b3860118396a68ba124fd24d4949
Sam Deacon <deaconsam@hotmail.co.uk>
2018-10-09 20:31:36 +0100

    removed auth from homeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,15 +8,6 @@ use Gate;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
      * Show the application dashboard.
